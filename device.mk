d07511f2eeef41fb15737df3d52cf8051fc245b1
Gowtham PJ <pjgowtham@gmail.com>
2020-03-25 09:48:52 +0530

    build android.frameworks.displayservice

--- device.mk
+++ device.mk
@@ -185,6 +185,7 @@ PRODUCT_COPY_FILES += \
 
 # Camera
 PRODUCT_PACKAGES += \
+    android.frameworks.displayservice@1.0 \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
     snap \
